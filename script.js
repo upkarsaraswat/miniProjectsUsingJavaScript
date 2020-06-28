6d7cebef861717773fe9a6295af4b8f64612bb1b
Upkar Saraswat <33596827+upkarsaraswat@users.noreply.github.com>
2020-06-28 14:59:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,151 +1,71 @@
-var ball = document.getElementById("ball");
-
-ball.style.top = ball.offsetTop + "px";
-ball.style.left = ball.offsetLeft + "px";
+var countInterval;
 
 
+function startCounter() {
+    
+    var number = parseInt(document.getElementById("number").value);
+    
+    if(isNaN(number)) {
+        alert("Please enter a number");
+        clearInterval(countInterval);   // This is important for the condition when a counter is running and you entered a wrong input for a new counter
+        return;
+    }
+    if(number < 1 || number > 99999) {
+        alert("Range out of bounds");
+        clearInterval(countInterval);
+        return;
+    }
+    
+    var currentNos = document.querySelectorAll(".counter .current");
+    var nextNos = document.querySelectorAll(".counter .next");
+    var count = 0;
+    
+    // If user clicks on 'Start Counter' button again - remove this function and below line if you don't consider this situation
+    resetNumbers(currentNos, nextNos, 5);
+    
+    // Clears the previous interval that was running
+    clearInterval(countInterval);
+    
+    countInterval = setInterval(function() {
+        if(count === number) {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+        increaseCount(currentNos, nextNos, 4);
+        count++;
+    }, 1000);
 
-// If you want dynamic change in values of ball height and width uncomment the below 'onresize' code.
-var ballHeight = ball.offsetHeight;
-var ballWidth = ball.offsetWidth;
-
-/*
-window.onresize = function (e) {
-    ballHeight = ball.offsetHeight;
-    ballWidth = ball.offsetWidth;
-};
-*/
+}
 
 
-
-function setValue(value) {
-    return value + "px";
+function resetNumbers(currentNos, nextNos, end) {
+    for(var i=0; i<end; ++i) {
+        currentNos[i].innerText = 0;
+        nextNos[i].innerText = 1;
+    }
 }
 
 
 
-// When using event.keyCode;
-function keyCode(keyPress) {
-
-    var top = parseInt(ball.style.top);
-    var left = parseInt(ball.style.left);
-
-    // W is pressed
-    if (keyPress === 119 || keyPress === 87) {
-        if (top > 5) {
-            ball.style.top = setValue(top - 5);
-        }
-    }
-
-    // A is pressed
-    else if (keyPress === 97 || keyPress === 65) {
-        if (left > 5) {
-            ball.style.left = setValue(left - 5);
-        }
-    }
-
-    // S is pressed
-    else if (keyPress === 115 || keyPress === 83) {
-        if (top < (window.innerHeight - ballHeight) - 5) {
-            ball.style.top = setValue(top + 5);
-        }
-    }
-
-    // D is pressed
-    else if (keyPress === 100 || keyPress === 68) {
-        if (left < (window.innerWidth - ballWidth) - 5) {
-            ball.style.left = setValue(left + 5);
-        }
-    }
-
-};
-
-
-
-/*
-//When using event.key;
-function key(keyPress) {
+function increaseCount(currentNos, nextNos, index) {
     
-    var top = parseInt(ball.style.top);
-    var left = parseInt(ball.style.left);
-
-    // W is pressed
-    if (keyPress === "w" || keyPress === "W") {
-        if (top > 5) {
-            ball.style.top = setValue(top - 5);
-        }
-    }
-
-    // A is pressed
-    else if (keyPress === "a" || keyPress === "A") {
-        if (left > 5) {
-            ball.style.left = setValue(left - 5);
-        }
-    }
-
-    // S is pressed
-    else if (keyPress === "s" || keyPress === "S") {
-        if (top < (window.innerHeight - ballHeight) - 5) {
-            ball.style.top = setValue(top + 5);
-        }
-    }
-
-    // D is pressed
-    else if (keyPress === "d" || keyPress === "D") {
-        if (left < (window.innerWidth - ballWidth) - 5) {
-            ball.style.left = setValue(left + 5);
-        }
-    }
-
-};
-*/
-
-
-
-/*
-//When using event.code;
-function code(keyPress) {
+    let current = currentNos[index];
+    let next = nextNos[index];
     
-    console.log(keyPress);
-
-    var top = parseInt(ball.style.top);
-    var left = parseInt(ball.style.left);
-
-    // W is pressed
-    if (keyPress === "KeyW") {
-        if (top > 5) {
-            ball.style.top = setValue(top - 5);
-        }
+    if(current.innerText == 9) {
+        increaseCount(currentNos, nextNos, index-1);
     }
-
-    // A is pressed
-    else if (keyPress === "KeyA") {
-        if (left > 5) {
-            ball.style.left = setValue(left - 5);
+    
+    next.classList.add("animate");
+    
+    setTimeout(function() {
+        current.innerText = next.innerText;
+        next.classList.remove("animate");
+        next.innerText = parseInt(next.innerText) + 1;
+        if(next.innerText > 9) {
+            next.innerText = 0;
         }
-    }
-
-    // S is pressed
-    else if (keyPress === "KeyS") {
-        if (top < (window.innerHeight - ballHeight) - 5) {
-            ball.style.top = setValue(top + 5);
-        }
-    }
-
-    // D is pressed
-    else if (keyPress === "KeyD") {
-        if (left < (window.innerWidth - ballWidth) - 5) {
-            ball.style.left = setValue(left + 5);
-        }
-    }
-
-};
-*/
-
-
-
-window.addEventListener("keypress", function (event) {
-//    code(event.code); //- You can use this
-//    key(event.key); //- You can also use this
-    keyCode(event.keyCode);
-});
+    }, 500);
+    
+}
